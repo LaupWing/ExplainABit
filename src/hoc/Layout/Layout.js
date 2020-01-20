2e1b790c902e13f9f7c7daf78a5494d2a2941675
LaupWing <loc_strict@hotmail.com>
2020-01-20 21:25:47 +0100

    changes

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import Aux from '../Auxilliry/Auxilliry'
+
+class Layout extends Component{
+    render(){
+        return(
+            <Aux>
+                
+            </Aux>
+        )
+    }
+}
+export default Layout 
