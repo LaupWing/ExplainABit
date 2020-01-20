4968d10fa0a40e05947bbeb3182b1ce44fdebe3d
LaupWing <loc_strict@hotmail.com>
2020-01-21 00:02:18 +0100

    adde dmy own logo

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import classes from './Navigation.module.css'
+import Searchbar from '../Searchbar/Searchbar'
+
 export default (props)=>(
     <nav className={classes.Navigation}>
-
+        <Searchbar/>
     </nav>
 )
