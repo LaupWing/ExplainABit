05d3636f2aa398e622d93d45b4b53201082900b6
LaupWing <loc_strict@hotmail.com>
2020-01-21 00:44:19 +0100

    added a good looking nav

---
+++ src/components/Navigation/Items/Items.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Item from './Item/Item'
+export default ()=>(
+    <ul>
+        <Item>
+            Contact
+        </Item>
+        <Item>
+            Questions
+        </Item>
+        <Item>
+            News
+        </Item>
+    </ul>
+)
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,12 +2,13 @@ import React from 'react'
 import classes from './Navigation.module.css'
 import Searchbar from './Searchbar/Searchbar'
 import Logo from '../Logo/Logo'
-
+import Items from './Items/Items'
 export default ()=>(
     <nav className={classes.Navigation}>
         <div className={classes.left}>
             <Logo logoType='nav'/>
             <Searchbar/>
         </div>
+        <Items/>
     </nav>
 )
