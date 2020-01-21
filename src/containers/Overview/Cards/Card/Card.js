b3f5643a839bfdd544b9e833c1febdaa0b10d40c
LaupWing <loc_strict@hotmail.com>
2020-01-21 18:31:27 +0100

    showing animatino works

--- src/containers/Overview/Cards/Card/Card.js
+++ src/containers/Overview/Cards/Card/Card.js
@@ -1,31 +1,58 @@
-import React, {Suspense, lazy} from 'react'
+import React, {Component,Suspense, lazy} from 'react'
 import moment from 'moment'
 import classes from './Card.module.css'
 import Button from '../../../../components/UI/Button/Button'
 
-export default (props)=>{
-    const CardComponent = lazy(()=>import(`./ComponentLib/${props.meta.name}/${props.meta.name}`))
-    const daysAgo = moment(props.date).fromNow() === 'a few seconds ago' ? 'today' : moment(props.date).fromNow()
-    
-    return (
-        <div className={classes.Card}>
-            <div className={classes.content}>
-                <header>
-                    <h2 className='name'>{props.meta.name}</h2>
-                    <h2 className={classes.added}><span>Added: </span>{daysAgo}</h2>
-                </header>
-                <div className={classes.description}>
-
+export default class extends Component{
+    state={
+        showDescription: false,
+        hidingClass: null
+    }
+    toggleDescription=()=>{
+        // if(this.state.showDescription){
+        //     this.setState({
+        //         hidingClass: 'hide'
+        //     })
+        // }
+        this.setState({
+            showDescription: !this.state.showDescription
+        })
+    }
+    animationEndHandler= (e)=>{
+        console.log('animationended',e)
+    }
+    render(){
+        const CardComponent = lazy(()=>import(`./ComponentLib/${this.props.meta.name}/${this.props.meta.name}`))
+        const daysAgo = moment(this.props.date).fromNow() === 'a few seconds ago' ? 'today' : moment(this.props.date).fromNow()
+        return (
+            <div className={classes.Card}>
+                <div className={classes.content}>
+                    <header>
+                        <h2 className='name'>{this.props.meta.name}</h2>
+                        <h2 className={classes.added}><span>Added: </span>{daysAgo}</h2>
+                    </header>
+                    
+                    {!this.state.showDescription
+                        ?   <div className={classes.componentContainer}>
+                                <Suspense fallback={<div>Loading...</div>}>
+                                    <CardComponent/>
+                                </Suspense>
+                            </div>
+                        :   <div 
+                                className={`${classes.description} ${classes[this.state.hidingClass]}`}
+                                onAnimationEnd={this.animationEndHandler}
+                            >
+                            <p>{this.props.meta.description}</p>
+                        </div>
+                    }
+                    <Button 
+                        btnType={!this.state.showDescription ? 'showDescription' : 'hideDescription'}
+                        clicked={this.toggleDescription}
+                    >
+                        {!this.state.showDescription ? 'Show description' : 'Hide description'}
+                    </Button>
                 </div>
-                <div className={classes.componentContainer}>
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <CardComponent/>
-                    </Suspense>
-                </div>
-                <Button btnType='description'>
-                    Show description
-                </Button>
             </div>
-        </div>
-    )
+        )
+    }
 }
--- src/store/reducers/componentMeta.js
+++ src/store/reducers/componentMeta.js
@@ -4,22 +4,22 @@ const initialState = {
         {
             name: 'Axios',
             date: [2020-1-21],
-            description: ''
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ipsum augue, dapibus non nisl eget, lobortis rhoncus nisl. Praesent nec dictum justo. Quisque non pharetra enim, auctor dapibus felis.'
         },
         {
             name: 'Axios',
             date: [2020-1-21],
-            description: ''
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ipsum augue, dapibus non nisl eget, lobortis rhoncus nisl. Praesent nec dictum justo. Quisque non pharetra enim, auctor dapibus felis.'
         },
         {
             name: 'Axios',
             date: [2020-1-21],
-            description: ''
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ipsum augue, dapibus non nisl eget, lobortis rhoncus nisl. Praesent nec dictum justo. Quisque non pharetra enim, auctor dapibus felis.'
         },
         {
             name: 'Axios',
             date: [2020-1-21],
-            description: ''
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ipsum augue, dapibus non nisl eget, lobortis rhoncus nisl. Praesent nec dictum justo. Quisque non pharetra enim, auctor dapibus felis.'
         },
     ]
 }
