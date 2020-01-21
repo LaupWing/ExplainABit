3b09ec098f407939fad8a7499d691863e80a803c
LaupWing <loc_strict@hotmail.com>
2020-01-21 23:13:46 +0100

    fixed the added ago

--- src/containers/Overview/Cards/Card/Card.js
+++ src/containers/Overview/Cards/Card/Card.js
@@ -24,7 +24,7 @@ export default class extends Component{
     
     render(){
         const CardComponent = lazy(()=>import(`./ComponentLib/${this.props.meta.name}/${this.props.meta.name}`))
-        const daysAgo = moment(this.props.date).fromNow() === 'a few seconds ago' ? 'today' : moment(this.props.date).fromNow()
+        const daysAgo = moment(this.props.meta.date).fromNow()
         return (
             <div className={classes.Card}>
                 <div className={classes.content}>
--- src/store/reducers/componentMeta.js
+++ src/store/reducers/componentMeta.js
@@ -3,22 +3,22 @@ const initialState = {
     componentMeta:[
         {
             name: 'Axios',
-            date: [2020-1-21],
+            date: [2020,0,21],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ipsum augue, dapibus non nisl eget, lobortis rhoncus nisl. Praesent nec dictum justo. Quisque non pharetra enim, auctor dapibus felis.'
         },
         {
             name: 'Axios',
-            date: [2020-1-21],
+            date: [2020,0,20],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ipsum augue, dapibus non nisl eget, lobortis rhoncus nisl. Praesent nec dictum justo. Quisque non pharetra enim, auctor dapibus felis.'
         },
         {
             name: 'Axios',
-            date: [2020-1-21],
+            date: [2020,0,19],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ipsum augue, dapibus non nisl eget, lobortis rhoncus nisl. Praesent nec dictum justo. Quisque non pharetra enim, auctor dapibus felis.'
         },
         {
             name: 'Axios',
-            date: [2020-1-21],
+            date: [2020,0,21],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ipsum augue, dapibus non nisl eget, lobortis rhoncus nisl. Praesent nec dictum justo. Quisque non pharetra enim, auctor dapibus felis.'
         },
     ]
