bf199bb0f0e881169b0e9c57a98c5ecc284db8aa
LaupWing <loc_strict@hotmail.com>
2020-01-22 00:12:31 +0100

    added md

--- src/store/reducers/componentMeta.js
+++ src/store/reducers/componentMeta.js
@@ -1,27 +1,7 @@
 import * as actionTypes from '../actions/actionTypes'
+import data from './data'
 const initialState = {
-    componentMeta:[
-        {
-            name: 'Axios',
-            date: [2020,0,21],
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ipsum augue, dapibus non nisl eget, lobortis rhoncus nisl. Praesent nec dictum justo. Quisque non pharetra enim, auctor dapibus felis.'
-        },
-        {
-            name: 'Axios',
-            date: [2020,0,20],
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ipsum augue, dapibus non nisl eget, lobortis rhoncus nisl. Praesent nec dictum justo. Quisque non pharetra enim, auctor dapibus felis.'
-        },
-        {
-            name: 'Axios',
-            date: [2020,0,19],
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ipsum augue, dapibus non nisl eget, lobortis rhoncus nisl. Praesent nec dictum justo. Quisque non pharetra enim, auctor dapibus felis.'
-        },
-        {
-            name: 'Axios',
-            date: [2020,0,21],
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ipsum augue, dapibus non nisl eget, lobortis rhoncus nisl. Praesent nec dictum justo. Quisque non pharetra enim, auctor dapibus felis.'
-        },
-    ]
+    componentMeta:data
 }
 
 const reducer =(state=initialState, action)=>{
---
+++ src/store/reducers/data.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        name: 'Axios',
+        date: [2020,0,21],
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ipsum augue, dapibus non nisl eget, lobortis rhoncus nisl. Praesent nec dictum justo. Quisque non pharetra enim, auctor dapibus felis.'
+    },
+    {
+        name: 'Axios',
+        date: [2020,0,20],
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ipsum augue, dapibus non nisl eget, lobortis rhoncus nisl. Praesent nec dictum justo. Quisque non pharetra enim, auctor dapibus felis.'
+    },
+    {
+        name: 'Axios',
+        date: [2020,0,19],
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ipsum augue, dapibus non nisl eget, lobortis rhoncus nisl. Praesent nec dictum justo. Quisque non pharetra enim, auctor dapibus felis.',
+        readme: null
+    },
+    {
+        name: 'Axios',
+        date: [2020,0,21],
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ipsum augue, dapibus non nisl eget, lobortis rhoncus nisl. Praesent nec dictum justo. Quisque non pharetra enim, auctor dapibus felis.'
+    },
+]
