b69eb7da364a6da08429c1da3ba217036bb6560f
LaupWing <loc_strict@hotmail.com>
2020-01-24 13:57:19 +0100

    minor styling update

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,11 +3,14 @@ import classes from './Navigation.module.css'
 import Searchbar from './Searchbar/Searchbar'
 import Logo from '../Logo/Logo'
 import Items from './Items/Items'
+import {Link} from 'react-router-dom'
 
 export default ()=>(
     <nav className={classes.Navigation}>
         <div className={classes.left}>
-            <Logo logoType='nav'/>
+            <Link to="/">
+                <Logo logoType='nav'/>
+            </Link>
             <Searchbar/>
         </div>
         <Items/>
