22b630e533be800e18c78df95856d220f2740a3a
LaupWing <loc_strict@hotmail.com>
2020-01-25 15:02:52 +0100

    error page with back llink

--- src/containers/ErrorPage/ErrorPage.js
+++ src/containers/ErrorPage/ErrorPage.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import Error from '../../components/Error/Error'
 import Aux from '../../hoc/Auxilliry/Auxilliry'
 import classes from './ErrorPage.module.css'
+import {Link} from 'react-router-dom'
 
 class ErrorPage extends Component{
 
@@ -9,7 +10,9 @@ class ErrorPage extends Component{
         return(
             <Aux>
                 <h2>Error 404: Page not found</h2>
-                <p>Go back to the main page</p>
+                <Link to="/">
+                    <p>Go back to the main page</p>
+                </Link>
                 <div className={classes.svgwrapper}>
                     <Error/>
                 </div>
