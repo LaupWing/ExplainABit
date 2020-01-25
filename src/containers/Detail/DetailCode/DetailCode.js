72620e64da357f171ac117f89327bcaf9e7f60be
LaupWing <loc_strict@hotmail.com>
2020-01-25 13:16:49 +0100

    succesfully loaded the script file from atnoehr source

--- src/containers/Detail/DetailCode/DetailCode.js
+++ src/containers/Detail/DetailCode/DetailCode.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import classes from './DetailCode.module.css'
-import codes from '../../../ComponentLib/codes'
 import Highlight from 'react-highlight'
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { atelierCaveDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
@@ -8,26 +7,20 @@ import Loader from '../../../components/UI/Loader/Loader'
 import codesIndex from '../../../ComponentLib/codesIndex'
 
 export default (props)=>{
-    const componentCode = codes.find(c=>c.name === props.meta.name)
-
-    const fileNav = componentCode.codes.map(code=>{
-        let liClasses = null
-        if(props.activeFile){
-            if(props.activeFile.fileName === code.fileName){
-                liClasses = 'active'
-            }
-        }
-        return (
-            <li onClick={()=>props.clicked(code)} className={classes[liClasses]} key={code.fileName}>{code.fileName}</li>
-        )
-    })
-
     let code = null
-
-    console.log(codesIndex[props.meta.name])
-
-    if(props.activeFile){
-        const findCodeActiveFile = componentCode.codes.find(c=>c.fileName===props.activeFile.fileName)
+    if(props.activeFile && codesIndex[props.meta.name]){
+        const fileNav = codesIndex[props.meta.name].map(code=>{
+            let liClasses = null
+            if(props.activeFile){
+                if(props.activeFile.fileName === code.fileName){
+                    liClasses = 'active'
+                }
+            }
+            return (
+                <li onClick={()=>props.clicked(code)} className={classes[liClasses]} key={code.fileName}>{code.fileName}</li>
+            )
+        })
+        const findCodeActiveFile = codesIndex[props.meta.name].find(c=>c.fileName===props.activeFile.fileName)
         let initialWhiteSpace = null 
         const tabFixed = findCodeActiveFile
             .code.split(/\n/)
@@ -53,13 +46,22 @@ export default (props)=>{
                     {tabFixed}
                 </SyntaxHighlighter>
             )
+        return(
+            <div className={classes.DetailCode}>
+                <nav className={classes.FileNav}>{fileNav}</nav>
+                <main>
+                    {code}
+                </main>
+            </div>
+        )
+    }else {
+        return (
+            <div className={classes.DetailCode}>
+                <nav className={classes.FileNav}><li>Loading</li></nav>
+                <main className={classes.centering}>
+                    <Loader/>
+                </main>
+            </div>
+        )
     }
-    return(
-        <div className={classes.DetailCode}>
-            <nav className={classes.FileNav}>{fileNav}</nav>
-            <main>
-                {code}
-            </main>
-        </div>
-    )
 }
--- src/containers/Detail/DetailReadme/DetailReadme.js
+++ src/containers/Detail/DetailReadme/DetailReadme.js
@@ -2,16 +2,24 @@ import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import classes from './DetailReadme.module.css'
 import CodeBlock from './CodeBlock'
-
+import Loader from '../../../components/UI/Loader/Loader'
 export default (props)=>{
-    return(
-        <div className={classes.DetailReadme}>
-            <ReactMarkdown 
-                source={props.readme}
-                renderers={{
-                    code: CodeBlock
-                }}
-            />
-        </div>
-    )
+    if(props.readme){
+        return(
+            <div className={classes.DetailReadme}>
+                <ReactMarkdown 
+                    source={props.readme}
+                    renderers={{
+                        code: CodeBlock
+                    }}
+                />
+            </div>
+        )
+    }else{
+        return (
+            <div className={[classes.DetailReadme, classes.loading].join(' ')}>
+                <Loader/>
+            </div>
+        )
+    }
 }
