78c9d7f4e9246a0f9b5bacc18926f9112cbaf846
LaupWing <loc_strict@hotmail.com>
2020-01-27 23:47:34 +0100

    trying to make a filewatcher

--- filewatcher.js
+++ filewatcher.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const folderPath = './src/ComponentLib' 
 let totalFiles = null
+
 const {
     app,
     codes,
@@ -18,6 +19,35 @@ const getAllFiles = ()=>{
     })
 }
 
+const addNewCodeExporter = (name) =>{
+    const data = fs.readFileSync(`${folderPath}/codesIndex.js`).toString().split('\n')
+    data.splice(data.length-2, 0, `Codes['${name}'] = require('./${name}/code').default;\r`)
+    return data.join('\n')
+}
+const removeItemFromCodeExporter = (name) =>{
+    const data = fs.readFileSync(`${folderPath}/codesIndex.js`).toString().split('\n')
+    const filterOut = data.filter(item=>!item.includes(name))
+    return filterOut
+}
+
+    // {
+    //     name: 'Axios',
+    //     date: [2020,0,21],
+    //     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ipsum augue, dapibus non nisl eget, lobortis rhoncus nisl. Praesent nec dictum justo. Quisque non pharetra enim, auctor dapibus felis.'
+    // }
+
+const addToStoreData = (name)=>{
+    const data = fs.readFileSync('./src/store/reducers/data.js').toString()
+    console.log(data.replace('export default ', ''))
+    const newObj ={
+        name: 'Dummy',
+        date: [2020,0,21],
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ipsum augue, dapibus non nisl eget, lobortis rhoncus nisl. Praesent nec dictum justo. Quisque non pharetra enim, auctor dapibus felis.'
+    }
+    console.log(data.split('},').splice(da))
+}
+console.log(addToStoreData())
+
 fs.watch(folderPath, (eventType, filename) => {
     const latestSnapshot = totalFiles
     const totalFilesNow = getAllFiles()
@@ -25,22 +55,23 @@ fs.watch(folderPath, (eventType, filename) => {
         .length
     if(eventType === 'change')  return
     if(totalFilesNow > latestSnapshot){
-        fs.writeFile(`${folderPath}/${filename}/index.js`, indexjs, (err)=>{
-            if(err) return console.log(err)
-            console.log(`Succesfully wrote a index.js file in ${filename}`)
-        })
-        fs.writeFile(`${folderPath}/${filename}/App.js`, app, (err)=>{
-            if(err) return console.log(err)
-            console.log(`Succesfully wrote a App.js file in ${filename}`)
-        })
-        fs.writeFile(`${folderPath}/${filename}/README.md`, readme, (err)=>{
-            if(err) return console.log(err)
-            console.log(`Succesfully wrote a README.md file in ${filename}`)
-        })
-        fs.writeFile(`${folderPath}/${filename}/code.js`, codes, (err)=>{
-            if(err) return console.log(err)
-            console.log(`Succesfully wrote a code.js file in ${filename}`)
-        })
+        // fs.writeFile(`${folderPath}/${filename}/index.js`, indexjs, (err)=>{
+        //     if(err) return console.log(err)
+        //     console.log(`Succesfully wrote a index.js file in ${filename}`)
+        // })
+        // fs.writeFile(`${folderPath}/${filename}/App.js`, app, (err)=>{
+        //     if(err) return console.log(err)
+        //     console.log(`Succesfully wrote a App.js file in ${filename}`)
+        // })
+        // fs.writeFile(`${folderPath}/${filename}/README.md`, readme, (err)=>{
+        //     if(err) return console.log(err)
+        //     console.log(`Succesfully wrote a README.md file in ${filename}`)
+        // })
+        // fs.writeFile(`${folderPath}/${filename}/code.js`, codes, (err)=>{
+        //     if(err) return console.log(err)
+        //     console.log(`Succesfully wrote a code.js file in ${filename}`)
+        // })
+
     }
     totalFiles = totalFilesNow
 })
