7cf4a01ca63f3f36969c4b769eb271b04a9fd4cd
LaupWing <loc_strict@hotmail.com>
2020-01-30 21:43:30 +0100

    finished output backdrop plus modal

---
+++ src/ComponentLib/BackdropWithModal/Modal/Modal.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Backdrop from '../Backdrop/Backdrop'
+import styles from './Modal.module.css'
+
+export default (props)=>{
+    return(
+        <React.Fragment>
+            <Backdrop clicked={props.modalClose} show={props.show}/>
+            <div 
+                className={styles['Modal']}
+                style={{
+                    transform: props.show ? 'translateY(0)' : 'translateY(-500%)',
+                    opacity: props.show ? '1' : '0'
+                }} 
+            >
+                {props.children}
+            </div>
+        </React.Fragment>
+    )
+}
